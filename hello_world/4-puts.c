baccf80698a38e620cb4f071d8322b188b5e236c
manaralthqfiu <9veio677@gmail.com>
2025-10-13 10:02:17 +0000

    Add 4-puts.c program and update README

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main-Entry point
+ *
+ * Description: prints "Programming is like building a multilingunal puzzle"
  *
  * Description: prints "Programming is like a multilingual puzzle"
  *
  * Return: Always 0 (Success)
- */
-int main(void)
+*/
+init main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
