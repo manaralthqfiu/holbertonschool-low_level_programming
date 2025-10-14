6effd94d8828fb1ba728be4ac738e53c76490506
manaralthqfiu <9vieo677@gmail.com>
2025-10-14 20:31:16 +0000

    Fix output format for 1-last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,16 +9,13 @@
  */
 int main(void)
 {
-	int n;
-	int last_digit;
+	int n = 98;
+	int last_digit = n % 10;
 
-	srand(time(0));
-	n = rand();
-	last_digit = n % 10;
 	printf("Last digit of %d is %d ", n, last_digit);
 	if (last_digit > 5)
 {
-		printf("and is greater than 5\n");
+	printf("and is greater than 5\n");
 }
 	else if (last_digit == 0)
 {
