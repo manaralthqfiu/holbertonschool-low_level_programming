cce1ab737d9724f0cc0ad3fc7458238ef47afaa1
manaralthqfiu <9vieo677@gmail.com>
2025-10-20 13:11:23 +0000

    add

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,14 @@
 
 /**
  * _isupper - checks for uppercase character
+ * @c: The character to check (as an int)
+ *
  * Return: 1 if c is uppercase, 0 otherwise
 */
-int _isupper(int c)
+int _isupper(int c);
 {
 	if (c >= 'A' && c <= 'Z')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
