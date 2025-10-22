4295499a4b03ab87241c770c11d4f620b8f863ec
manaralthqfiu <9vieo677@gmail.com>
2025-10-22 08:46:20 +0000

    add

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using the character #
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, space, hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+	for (space = 0; space < size - row; space++)
+	}
+	_putchar(' ');
+}
+
+for (hash = 0; hash < row; hash++)
+{
+	_putchar('\n')
+}
+}
