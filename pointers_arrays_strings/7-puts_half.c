ae31b02fac2aed4021caab48c558c884739308c0
manaralthqfiu <9vieo677@gmail.com>
2025-10-25 12:06:17 +0000

    add

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string followed by a new line
+ * @str: pointer to the string
+ */
+void puts_half(char *str)
+{
+	int len = 0, i, start;
+
+	while (str[len] != '\0')
+		len++;
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
