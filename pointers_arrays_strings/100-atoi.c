ec540d798f2581645ce74f0d64181ec6435aaef4
manaralthqfiu <9vieo677@gmail.com>
2025-10-25 12:29:53 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -19,6 +19,13 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			if (started && result > (2147483647 - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
 			result = result * 10 + (s[i] - '0');
 			started = 1;
 		}
