0d3b72a4461dae19aa100f7d5124a4d6637c26fc
manaralthqfiu <9vieo677@gmail.com>
2025-10-25 12:22:34 +0000

    add

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -19,13 +19,22 @@ int _atoi(char *s)
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
+			digit_started = 1;
+
+			if (result > (2147483647 - (s[i] - '0')) / 10)
+			{
+				if (sign == 1)
+					return (2147483647);
+				else
+					return (-2147483648);
+			}
+
 			result = result * 10 + (s[i] - '0');
-			found_digit = 1;
 		}
-		else if (found_digit)
+		else if (digit_started)
 			break;
+
 		i++;
 	}
-
 	return (result * sign);
 }
