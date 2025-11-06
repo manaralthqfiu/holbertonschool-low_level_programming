94756dad3e90f78f4f5e96dc7ee4356742a494c8
manaralthqfiu <9vieo677@gmail.com>
2025-11-06 09:31:51 +0000

    add

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,12 +13,12 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concat;
 	int i, j, len1, len2;
-	
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
@@ -32,7 +32,6 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; i < len1; i++)
 		concat[i] = s1[i];
-	
 	for (j = 0; j < len2; j++)
 		concat[i + j] = s2[j];
 
