794befe1ccafb05100703e919861e8af4a859a80
manaralthqfiu <9vieo677@gmail.com>
2025-11-07 15:00:34 +0000

    add

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
-	if (n => len2)
+	if (n >= len2)
 		n = len2;
 
 	result = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -34,7 +34,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (i = 0; i < len1; i++)
 		result[i] = s1[i];
-	for (j = 0; j < len2; j++)
+	for (j = 0; j < n; j++)
 		result[i + j] = s2[j];
 
 	result[i + j] = '\0';
