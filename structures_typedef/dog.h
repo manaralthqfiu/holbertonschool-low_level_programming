731ccf997299276f3309a60d00e19b80aefd7a3d
manaralthqfiu <9vieo677@gmail.com>
2025-11-08 15:45:09 +0000

    add

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,14 +6,26 @@
  * @age: Age of the dog
  * @owner: Name of the owner
  */
-struct dog
+typedef struct dog.
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
+/**
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to struct dog to initialize
+ * @name: Name to assign
+ * @age: Age to assign
+ * @owner: Owner to assign
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/**
+ * print_dog - Prints the contents of a struct dog
+ * @d: Pointer to struct dog to print
+ */
 void print_dog(struct dog *d);
 
 #endif
