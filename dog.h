f8a090a36ce710366519f620d30812cff1626634
manaralthqfiu <9vieo677@gmail.com>
2025-11-08 15:10:35 +0000

    add

---
+++ dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - A structure to store dog information
+ * @name: Pointer to the dog's name (string)
+ * @age: Dog's age (float)
+ * @owner: Pointer to the owner's name (string)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
