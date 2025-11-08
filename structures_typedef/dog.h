1126f2b0ff8436df4da29bf21b15d40ad873e8dc
manaralthqfiu <9vieo677@gmail.com>
2025-11-08 15:25:52 +0000

    add

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Structure that stores dog information
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ */
 struct dog
 {
 	char *name;
