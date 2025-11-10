57b819ae0145a87745aa9f611f6b1ed0cba3854b
manaralthqfiu <9vieo677@gmail.com>
2025-11-10 10:39:08 +0000

    add

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,9 +13,9 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
-	for (i = 0; i < sze; i++)
+	for (i = 0; i < size; i++)
 	{
-		if (cpm(array[i]) != 0)
+		if (cmp(array[i]) != 0)
 			return (i);
 }
 
