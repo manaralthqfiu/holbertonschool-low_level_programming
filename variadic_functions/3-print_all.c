5a41686eca6054f132b166fc5e6ac4593a4c3022
manaralthqfiu <9vieo677@gmail.com>
2025-11-15 17:20:43 +0000

    add

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -30,12 +30,13 @@ void print_all(const char * const format, ...)
 				break;
 			case 's':
 				str = va_arg(args, char *);
-				if (!str)
+				if (str == NULL)
 					str = "(nil)";
-				printf("%s%s", sep, str);
+				printf("%s", str);
 				break;
 		}
-		sep = ", ";
+		if (*sep == '\0')
+			sep = ", ";
 		i++;
 	}
 
