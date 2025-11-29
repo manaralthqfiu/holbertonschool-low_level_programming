8e119aba8db32a42f6d9d925e57cd4e20c6a6bd5
manaralthqfiu <9vieo677@gmail.com>
2025-11-29 19:25:58 +0000

    Add node at the end

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -28,4 +28,13 @@ size_t list_len(const list_t *h);
  */
 list_t *add_node(list_t **head, const char *str);
 
+/**
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: pointer to the head of the list
+ * @str: string to be duplicated and stored in the new node
+ *
+ * Return: address of the new element, or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str);
+
 #endif
